5b9b1b59dd43b82d98213d100fdb03d32be39278
Brandon <brandonbgrayson@gmail.com>
2023-12-06 20:07:52 -0500

    placed hero image inside app page component from client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import { Anime } from "@/schemas/schemas";
 import { getHeroData } from "./actions/actions";
-import Background from "./Background";
+import Image from "next/image";
+import { relative } from "path";
 
-export default async function Home() {
+export default async function HomePage() {
   const response = await getHeroData();
   const data: Anime = response.data[0];
+  const synopsis = data.attributes.synopsis;
 
-  return <Background />;
+  return (
+    <main>
+      <div style={{ position: "relative", height: "100vh", width: "100vw" }}>
+        <Image
+          alt="One Piece crew"
+          src="/img/one-piece-crew.jpeg"
+          fill
+          sizes="100vw"
+          quality={100}
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
+    </main>
+  );
 }
