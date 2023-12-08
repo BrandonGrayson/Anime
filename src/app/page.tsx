f1ef3a0fedcc1b1401950255cc536bb0b2932f62
Brandon <brandonbgrayson@gmail.com>
2023-12-08 05:30:13 -0500

    background with text and sizing for mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
 import { Anime } from "@/schemas/schemas";
 import { getHeroData } from "./actions/actions";
 import Image from "next/image";
-import { relative } from "path";
+import styles from "./homestyles.module.css";
 
 export default async function HomePage() {
   const response = await getHeroData();
   const data: Anime = response.data[0];
   const synopsis = data.attributes.synopsis;
+  const title = data.attributes.titles.en;
 
   return (
     <main>
-      <div style={{ position: "relative", height: "100vh", width: "100vw" }}>
+      <div
+        style={{
+          position: "relative",
+          height: "100vh",
+        }}
+      >
+        <p
+          style={{
+            color: "white",
+            fontFamily: "sans-serif",
+            fontSize: "3em",
+            zIndex: 1,
+          }}
+          className={styles.heroTitle}
+        >
+          {title}
+        </p>
         <Image
           alt="One Piece crew"
           src="/img/one-piece-crew.jpeg"
@@ -19,6 +36,7 @@ export default async function HomePage() {
           quality={100}
           style={{
             objectFit: "cover",
+            zIndex: -1,
           }}
         />
       </div>
